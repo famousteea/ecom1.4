134753aeaf447ead95b06ed0683662dee4604cdc
moustapha <mbmoustapha4005@gmail.com>
2023-10-10 14:51:15 -0400

    ajouter

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <form method="get" action="result.php">
-            mot_de_passe:
-            <input type="text" name="fmdp">
-            <input type="submit">
-        </form>
-        
-    </body>
-</html>
+<?php
+$server='localhost';
+$username='root';
+$pwd='';
+$db='ecom_1';
+$connection=mysqli_connect($server,$username,$pwd,$db);
+if ($connection) {
+   echo"connected to the $db database succesfully";
+}else{
+    echo"Error :Not connected to the $db database";
+}
+$results=mysqli_query($connection,"SELECT * FROM userr");
+$data1=($rangeedata=mysqli_fetch_row($results));
+$data2=($rangeedata=mysqli_fetch_assoc($results));
+echo('</br>');
+echo('first fecth');
+echo('</br>');
+var_dump($data1);
+echo('</br>');
+echo('second fecth');
+echo('</br>');
+var_dump($data2);
+echo('</br>');
+echo('</br>');
+echo('second fecth');
+echo('</br>');
+?>
